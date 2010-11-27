ecfd2fd12ffbaf63f27ca2e9f3528fb4217dc8fa
Romain <romain@meltingmedia.net>
2010-11-27 13:18:14 +0100

    french translation attempt

---
+++ core/components/xdbedit/lexicon/fr/default.inc.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * XdbEdit
+ *
+ * Copyright 2010 by Bruno Perner <b.perner@gmx.de>
+ *
+ * This file is part of XdbEdit, for editing custom-tables in MODx Revolution CMP.
+ *
+ * XdbEdit is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * XdbEdit is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * XdbEdit; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA 
+ *
+ * @package xdbedit
+ */
+/**
+ * XdbEdit French language file
+ *
+ * @package xdbedit
+ * @subpackage lexicon
+ */
+$_lang['xdbedit'] = 'Custom-Table Editor';
+$_lang['xdbedit.edit'] = 'Éditer';
+$_lang['xdbedit.create'] = 'Créer';
+$_lang['xdbedit.publish'] = 'Publier';
+$_lang['xdbedit.unpublish'] = 'Dépublier';
+$_lang['xdbedit.bulk_actions'] = 'Actions groupées';
+$_lang['xdbedit.publish_selected'] = 'Publier la sélection';
+$_lang['xdbedit.unpublish_selected'] = 'Dépublier la sélection';
+$_lang['xdbedit.delete_selected'] = 'Supprimer la sélection';
+$_lang['xdbedit.remove_selected'] = 'Enlever la sélection';
+$_lang['angebote.edit'] = 'Modifier l\'offre';
+$_lang['angebote.create'] = 'Créer l\'offre';
+$_lang['angebote.publish'] = 'Publier l\'offre';
+$_lang['angebote.unpublish'] = 'Dépublier l\'offre';
+$_lang['month'] = 'Mois';
+$_lang['year'] = 'Année';
